5344578ec1805c051112bf042a76e9a5bf53976b
Daniel Lima <danielcorreiaborges9@hotmail.com>
2023-09-04 14:03:52 -0300

    correção automatica slint

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -5,8 +5,8 @@ import VeterinarioSchema from '../entity/VeterinarioSchema.js';
 const db = new DataSource({
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: 5432,
-  username: 'postgres',
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: 'db',
   synchronize: true,
