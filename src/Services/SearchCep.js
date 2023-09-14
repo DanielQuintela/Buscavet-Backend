0ea3b7590f167bee8ee28fa72707802ad51cc98e
Daniel Lima <danielcorreiaborges9@hotmail.com>
2023-09-14 00:01:59 -0300

    Service de Buscar CEP funcionando

--- src/Services/SearchCep.js
+++ src/Services/SearchCep.js
@@ -1,26 +1,29 @@
-import axios from 'axios';
+import GetCep from '../Infrastructure/GetCep.js';
 
 export default class SearchCep {
-    static searchCep = async (req, res) => {
-        try{
+  static searchCep = async (req, res) => {
+    try {
+      const { cep } = req.body;
 
-            const cep = req.body;
+      if (!cep || !/^\d{8}$/.test(cep)) {
+        return res.status(400).send({ message: 'CEP inválido' });
+      }
 
-            if (!cep || !/^\d{8}$/.test(cep)) {
-                return res.status(400).send({ message: 'CEP inválido' });
-              }
-              
-            const search = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      const search = await GetCep(cep);
 
-            if (search.status !== 200){
-                return res.status(500).send({ message: 'Erro ao buscar o CEP' });
-            }
-            
-            const data = search.data;
-            res.status(200).send(data);
+      if (search.status !== 200) {
+        return res.status(500).send({ message: 'Erro ao buscar o CEP' });
+      }
 
-        } catch (error) {
-            res.status(500).send({ message: error.message});
-        }
-    };
+      // TODO: CRIAR UM TRATAMENTO QUANDO O CEP VIER VAZIO OU NÃO ENCONTRADO
+      // if (search.erro === 'true') {
+      //   return { status: 204, data: { message: 'CEP não Encontrado' } };
+      // }
+
+      const { data } = search;
+      return res.status(200).send(data);
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
+  };
 }
--- src/routes/ServicesRouter.js
+++ src/routes/ServicesRouter.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import SearchCep from '../Services/SearchCep';
+import SearchCep from '../Services/SearchCep.js';
 
 const router = express.Router();
 
 router
-    .post('/services/getcep', SearchCep.searchCep);
+  .post('/services/getcep', SearchCep.searchCep);
 
 export default router;
