6cbee48903b19817dd2f14565d50aa364f1e1f61
Daniel Lima <danielcorreiaborges9@hotmail.com>
2023-09-27 20:29:51 -0300

    merge

--- src/entity/index.js
+++ src/entity/index.js
@@ -1,4 +1,22 @@
 import UsuarioSchema from './UsuarioSchema.js';
 import VeterinarioSchema from './VeterinarioSchema.js';
+import EspecializacaoSchema from './EspecializacaoSchema.js';
+import EstabelecimentoSchema from './EstabelecimentoSchema.js';
+import ServicoSchema from './ServicoSchema.js';
+import VacinaSchema from './VacinaSchema.js';
+import ProdutoSchema from './ProdutoSchema.js';
+import ClienteSchema from './ClienteSchema.js';
+import AnimalEstimacaoSchema from './AnimalEstimacaoSchema.js';
+import ConsultaSchema from './ConsultaSchema.js';
+import AvaliacaoSchema from './AvaliacaoSchema.js';
+import DoseVacinaAnimalSchema from './DoseVacinaAnimalSchema.js';
+import ProdutoEstabelecimentoSchema from './ProdutoEstabelecimentoSchema.js';
+import ServicoEstabelecimentoSchema from './ServicoEstabelecimentoSchema.js';
+import ServicoVeterinarioSchema from './ServicoVeterinarioSchema.js';
+import UsuarioEstabelecimentoSchema from './UsuarioEstabelecimentoSchema.js';
+import VeterinarioEstabelecimentoSchema from './VeterinarioEstabelecimentoSchema.js';
 
-export { UsuarioSchema, VeterinarioSchema };
+export {
+  // eslint-disable-next-line max-len
+  UsuarioSchema, VeterinarioSchema, EspecializacaoSchema, EstabelecimentoSchema, ServicoSchema, VacinaSchema, ProdutoSchema, ClienteSchema, AnimalEstimacaoSchema, ConsultaSchema, AvaliacaoSchema, DoseVacinaAnimalSchema, ProdutoEstabelecimentoSchema, ServicoEstabelecimentoSchema, ServicoVeterinarioSchema, UsuarioEstabelecimentoSchema, VeterinarioEstabelecimentoSchema,
+};
