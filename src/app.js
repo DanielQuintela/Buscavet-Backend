141ce712af2afaf1485121ebaa9c956d619932f8
Daniel Lima <danielcorreiaborges9@hotmail.com>
2023-10-16 18:43:24 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import express from 'express';
 import routes from './routes/index.js';
 
-const cors = require('cors');
-
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 routes(app);
 
